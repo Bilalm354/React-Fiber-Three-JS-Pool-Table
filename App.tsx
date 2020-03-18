c579fc355156569033d671b2fcef8b8b31b8fcbc
bilalm354 <bilalm354@gmail.com>
2020-03-18 00:53:28 +0000

    initial

---
+++ App.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Canvas } from "react-three-fiber";
+
+import Scene from "./views/Scene";
+import Controls from "./components/Controls";
+
+function App() {
+    return (
+        <Canvas>
+            <Scene />
+            <Controls />
+        </Canvas>
+    );
+}
+
+export default App;
